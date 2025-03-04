e5a47cd5ae088a44bda838f9179b685212c0c117
David Hoang <88292464+David-Hoang@users.noreply.github.com>
2025-03-04 13:56:52 +0100

    fix error post

--- index.js
+++ index.js
@@ -7,29 +7,28 @@ const port = 3000;
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 
-app.post('/pokemons'), (request, response) => {
+app.get('/', (request, response) => {
+    return response.end('Welcome to my first API');
+})
+
+app.get('/all-pokemon', (request, response) => {
+    return response.json(pokemons);
+})
+
+app.get('/pokemon/:id', (request, response) => {
+    let pokemonId = parseInt(request.params.id);
+    let pokemonById = pokemons.find(pokemon => pokemon.id === pokemonId);
+
+    if(pokemonId && pokemonById){
+        return response.status(200).json(pokemonById);
+    }else{
+        return response.status(404).json({error : 404, message : 'Pokemon not found'});
+    }
+})
+
+app.post('/pokemons', (request, response) => {
     console.log(request.body);
-}
-
-// app.get('/', (request, response) => {
-//     return response.end('Welcome to my first API');
-// })
-
-// app.get('/all-pokemon', (request, response) => {
-//     return response.json(pokemons);
-// })
-
-// app.get('/pokemon/:id', (request, response) => {
-//     let pokemonId = parseInt(request.params.id);
-//     let pokemonById = pokemons.find(pokemon => pokemon.id === pokemonId);
-
-//     if(pokemonId && pokemonById){
-//         return response.status(200).json(pokemonById);
-//     }else{
-//         return response.status(404).json({error : 404, message : 'Pokemon not found'});
-//     }
-// })
-
+});
 
 
 app.listen(port, () => {
