fbdd4211030073681eda49e08e88e933ffe1e44a
David Hoang <88292464+David-Hoang@users.noreply.github.com>
2025-03-04 12:03:48 +0100

    add single show

--- index.js
+++ index.js
@@ -6,11 +6,22 @@ const port = 3000;
 
 
 app.get('/', (request, response) => {
-    return response.end('Welcome to my first APIs');
+    return response.end('Welcome to my first API');
 })
 
 app.get('/all-pokemon', (request, response) => {
     return response.json(pokemons);
+})
+
+app.get('/pokemon/:id', (request, response) => {
+    let pokemonId = parseInt(request.params.id);
+    
+    if(pokemonId && pokemons.find(pokemon => pokemon.id === pokemonId)){
+        let pokemonById = pokemons.find(pokemon => pokemon.id === pokemonId);
+        return response.status(200).json(pokemonById);
+    }else{
+        return response.status(404).json({error : 404, message : 'Pokemon not found'});
+    }
 })
 
 app.listen(port, () => {
